eb5f59e20b75e26463bc29e291701dce7f125e78
AnZ4k <77916663+AnZ4k@users.noreply.github.com>
2021-07-18 23:07:14 -0400

    Refatoração no codigo

--- main.cpp
+++ main.cpp
@@ -2,23 +2,23 @@
 
 using namespace std; 
 
-int fatorial(int x); 
+int fatorial ( int x ); 
 
-int main()
+int main ( )
 {
     int n, resultado;
 
     cout << "Insira um numero: ";  
     cin >> n;                
 
-    resultado = fatorial(n); 
+    resultado = fatorial ( n ); 
     cout << "O fatorial de " << n << " é " << resultado << endl; 
     
     return 0;
 }
 
-int fatorial(int x)
+int fatorial ( int x )
 {
-    if (x == 0) return 1;
-    return x * fatorial(x - 1);
+    if ( x == 0 ) return 1;
+    return x * fatorial ( x - 1 );
 }
